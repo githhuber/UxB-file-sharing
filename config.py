a5c88002b9f76881c679c4100d991e99127b2197
githhuber <foronlyhoneygain@gmail.com>
2024-09-17 16:12:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE',"" )) # Add time in seconds
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE',"4084" )) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "")
 TUT_VID = os.environ.get("TUT_VID","")
 
