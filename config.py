c5ef689c6cfabcd140f68d85dcba54824707d16f
githhuber <foronlyhoneygain@gmail.com>
2024-09-17 18:46:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL")
-DB_NAME = os.environ.get("DATABASE_NAME")
+DB_NAME = "Main"
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL"))
