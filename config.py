508c1ff74abf28c8b00296b9be08b71b1d2975bb
githhuber <foronlyhoneygain@gmail.com>
2024-09-17 16:08:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "")
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", ""))
 FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", ""))
-FORCESUB_CHANNEL3 = int(os.environ.get("FORCESUB_CHANNEL3", ""))
+
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
